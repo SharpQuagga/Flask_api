aa47ba7a42a3ca5669e9da960f54844b2d7ebd8d
SharpQuagga <sharpquagga1998@gmail.com>
2019-07-23 15:44:29 +0530

    psycopg2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from flask_restful import Resource, Api
 from flask_jwt import JWT
 from security import authenticate, identity
@@ -7,7 +8,7 @@ from resources.item import Item, ItemList
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:password@127.0.0.1:3306/testdb'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
